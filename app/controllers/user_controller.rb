2a9f02dc894b55ce6b6b2d35d69ab2c276ad0a39
Michael Gervasoni <michaelgervasoni@gmail.com>
2018-04-27 20:52:53 -0700

    working registration that saves users to a user_list.txt file

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,10 +8,16 @@ class UserController < ApplicationController
   #   # @user = User.new
   # end
   def create
-  @user = User.new(params[:un],params[:pw],params[:fname],params[:lname])
+  @user = User.new(params[:un],params[:pw],params[:email],params[:fname],params[:lname], params[:city], params[:state], params[:zip],params[:country])
+  # @user1 = @user.each { |x| puts x }
+  # @user.attributes.each_pair do |name, value|
+  #   puts "#{name} = #{value}"
+  # end
   file = File.new("users.txt", "w+")
-  # file.write("[#{@user.un}, #{@user.pw}, #{@user.fname}, #{@user.lname}]")
-  file.write("#{@user.un},#{@user.pw},#{@user.fname},#{@user.lname},")
+  #file.write("[#{@user.un}, #{@user.pw}, #{@user.fname}, #{@user.lname}]")
+  # @user1 = @user.each { |x| puts x }
+  file.write("#{@user.un},#{@user.pw},#{@user.email},#{@user.fname},#{@user.lname},#{@user.other}")
+#  file.write("#{@user}")
 # w is write only
 # r+  read-write starts at beginning
 # w+  read-write and overwrites existing data (or creates new)
@@ -44,13 +50,14 @@ class UserController < ApplicationController
     @userArray = @readallfile.split(",")
     @userName = @userArray[0]
     @userPass = @userArray[1]
+    @testthing = @userArray.each { |x| puts x }
     # if @userName == params[:un] && @userPass == params[:pw]
         if @userPass == params[:pw]
         #Send flash notice
         flash.now[:notice] = "You're A FUCKING BOSS (aka credentials are valid)"
         #
-        file = File.new("users.txt", "w+")
-        file.write("#{@user.un},#{@user.pw},#{@user.fname},#{@user.lname},")
+        file = File.new("user_list.txt", "a")
+        file.write("#{@testthing} \n")
         file.close
         render('/home/homepage')
       else
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,27 @@
-class User 
-  attr_accessor :un, :pw, :fname, :lname
-  def initialize(un, pw, fname, lname)
+class User
+  # @@ count will keep count of the number of objects created:
+  @@count = 0
+  attr_accessor :un, :pw, :email, :fname, :lname, :city, :state, :zip, :country, :other
+  def initialize(un, pw, email, fname, lname, *other)
   @un = un
   @pw = pw
+  @email = email
   @fname = fname
   @lname = lname
+  @other = other.join(',')
+  @city = "city"
+  @state = "state"
+  @zip = "zip"
+  @country = "USA"
+  @@count +=1
   end
 
-def model_name
-  @un = un
+def self.get_count
+  @@count
 end
+#the User.get_count should return the number of created users
+# def model_name
+#   @un = un
+# end
 
 end
